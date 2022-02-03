7368c41205310e45584a219beb22ac4c750ed618
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-03 23:31:26 +0600

    factorial in js

--- lecture-4-leap-year.js
+++ lecture-4-leap-year.js
@@ -1,3 +1,7 @@
+// logic
+// as leap year repeat after 4 years then if it is divisible by 4 then it should be a leap year but there are exception thats why we also have to check that it isn't divisible by 100 also
+// or simply it is divisible by 400 then its a leap year :D 
+
 function isLeapYear(year) {
     if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
         return true;
