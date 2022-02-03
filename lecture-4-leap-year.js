9f20ee5e96388d653ae80777cb51a3d90db69ae3
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-02-03 23:01:26 +0600

    leap year

---
+++ lecture-4-leap-year.js
@@ -0,0 +1,11 @@
+function isLeapYear(year) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+var thisYear = isLeapYear(2024);
+console.log(thisYear);
